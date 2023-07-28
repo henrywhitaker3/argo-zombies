5ea9f04c7ef45e7898ba845a21d4e8da9cc22442
Henry Whitaker <henrywhitaker3@outlook.com>
2023-07-28 09:39:52 +0100

    added some aks exclusions

--- internal/exclusions/bundles/aks.go
+++ internal/exclusions/bundles/aks.go
@@ -5,7 +5,23 @@ import "github.com/henrywhitaker3/argo-zombies/internal/exclusions"
 func AksBundle() bundleFunc {
 	return func() exclusions.Exclusions {
 		e := exclusions.Exclusions{
-			Resources:             []exclusions.ExcludedResource{},
+			Resources: []exclusions.ExcludedResource{
+				{
+					Name:    "aks-node-validating-webhook",
+					Kind:    "ValidatingWebhookConfiguration",
+					Version: "admissionregistration.k8s.io/v1",
+				},
+				{
+					Name:    "aks-node-mutating-webhook",
+					Kind:    "MutatingWebhookConfiguration",
+					Version: "admissionregistration.k8s.io/v1",
+				},
+				{
+					Name:    "aks-webhook-admission-controller",
+					Kind:    "MutatingWebhookConfiguration",
+					Version: "admissionregistration.k8s.io/v1",
+				},
+			},
 			GroupVersionResources: []exclusions.ExcludedGroupVersionResource{},
 			Namespaces:            []string{},
 			Selectors:             []exclusions.ExcludedMetadata{},
