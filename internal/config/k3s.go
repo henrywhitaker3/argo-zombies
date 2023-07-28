b0415f1145d2d84117c164e2d57a6e9d2befb1ce
Henry Whitaker <henrywhitaker3@outlook.com>
2023-07-28 09:24:47 +0100

    move some k8s defaults out of k3s

--- internal/config/k3s.go
+++ internal/config/k3s.go
@@ -297,34 +297,6 @@ func k3sBundle() bundleFunc {
 					Kind:      "RoleBinding",
 					Version:   "rbac.authorization.k8s.io/v1",
 				},
-				{
-					Name:    "system-cluster-critical",
-					Kind:    "PriorityClass",
-					Version: "scheduling.k8s.io/v1",
-				},
-				{
-					Name:    "system-node-critical",
-					Kind:    "PriorityClass",
-					Version: "scheduling.k8s.io/v1",
-				},
-				{
-					Name:      "kubernetes",
-					Namespace: "default",
-					Kind:      "EndpointSlice",
-					Version:   "discovery.k8s.io/v1",
-				},
-				{
-					Name:      "kubernetes",
-					Namespace: "default",
-					Kind:      "Service",
-					Version:   "v1",
-				},
-				{
-					Name:      "kube-dns",
-					Namespace: "kube-system",
-					Kind:      "Service",
-					Version:   "v1",
-				},
 			},
 			GroupVersionResources: []ExcludedGroupVersionResource{
 				{
