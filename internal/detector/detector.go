41e0242ab21e597623687e548d21cf13f9155c72
Henry Whitaker <henrywhitaker3@outlook.com>
2025-10-30 21:01:54 +0000

    perf: process api groups concurrently
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- internal/detector/detector.go
+++ internal/detector/detector.go
@@ -2,6 +2,9 @@ package detector
 
 import (
 	"context"
+	"fmt"
+	"runtime"
+	"sync"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -10,15 +13,22 @@ import (
 	"k8s.io/utils/strings/slices"
 )
 
+type dynClientFunc func() (*dynamic.DynamicClient, error)
+
 type Detector struct {
 	client    *kubernetes.Clientset
-	dynClient *dynamic.DynamicClient
+	dynClient dynClientFunc
 }
 
-func NewDetector(client *kubernetes.Clientset, dynClient *dynamic.DynamicClient) *Detector {
+type DetectorOpts struct {
+	Client    *kubernetes.Clientset
+	DynClient dynClientFunc
+}
+
+func NewDetector(opts DetectorOpts) *Detector {
 	return &Detector{
-		client:    client,
-		dynClient: dynClient,
+		client:    opts.Client,
+		dynClient: opts.DynClient,
 	}
 }
 
@@ -34,7 +44,12 @@ func (d *Detector) Detect(ctx context.Context) (*Collection, error) {
 func (d *Detector) getResources(ctx context.Context) (*Collection, error) {
 	collection := NewCollection()
 
-	filters := BuildFilters()
+	groupCh := make(chan *metav1.APIResourceList, 100)
+
+	wg := &sync.WaitGroup{}
+	if err := d.setupHandlers(ctx, wg, collection, groupCh); err != nil {
+		return nil, fmt.Errorf("setup detector handler: %w", err)
+	}
 
 	_, list, err := d.client.ServerGroupsAndResources()
 	if err != nil {
@@ -42,49 +57,106 @@ func (d *Detector) getResources(ctx context.Context) (*Collection, error) {
 	}
 
 	for _, group := range list {
-		gv, err := schema.ParseGroupVersion(group.GroupVersion)
-		if err != nil {
-			return nil, err
-		}
-
-	rl:
-		for _, resource := range group.APIResources {
-			gvr := schema.GroupVersionResource{
-				Group:    gv.Group,
-				Version:  gv.Version,
-				Resource: resource.Name,
-			}
-
-			for _, skip := range getSkipList() {
-				if gvr == skip {
-					// fmt.Printf("skipping %s/%s/%s", gvr.Group, gvr.Version, gvr.Resource)
-					continue rl
-				}
-			}
-
-			if !slices.Contains(resource.Verbs, "list") {
-				continue
-			}
-			// fmt.Printf("processing resource %s/%s/%s\n", gv.Group, gv.Version, resource.Name)
-
-			restAPI := d.dynClient.Resource(gvr).Namespace("")
-
-			gvrList, err := restAPI.List(ctx, metav1.ListOptions{})
-			if err != nil {
-				continue
-			}
-
-		il:
-			for _, item := range gvrList.Items {
-				for _, filter := range filters {
-					if filter(item) {
-						continue il
-					}
-				}
-				collection.Push(item)
-			}
-		}
+		wg.Add(1)
+		groupCh <- group
 	}
 
+	wg.Wait()
+
 	return collection, nil
+}
+
+func (d *Detector) setupHandlers(
+	ctx context.Context,
+	wg *sync.WaitGroup,
+	col *Collection,
+	ch <-chan *metav1.APIResourceList,
+) error {
+	for range runtime.NumCPU() {
+		dynClient, err := d.dynClient()
+		if err != nil {
+			return fmt.Errorf("build detector dynamic client: %w", err)
+		}
+		go d.handleGroups(ctx, handleGroupOpts{
+			dynClient: dynClient,
+			col:       col,
+			wg:        wg,
+			ch:        ch,
+		})
+	}
+	return nil
+}
+
+type handleGroupOpts struct {
+	dynClient *dynamic.DynamicClient
+	col       *Collection
+	wg        *sync.WaitGroup
+	ch        <-chan *metav1.APIResourceList
+}
+
+func (d *Detector) handleGroups(ctx context.Context, opts handleGroupOpts) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case group := <-opts.ch:
+			d.processGroup(ctx, processGroupOpts{
+				dynClient: opts.dynClient,
+				col:       opts.col,
+				group:     group,
+			})
+			opts.wg.Done()
+		}
+	}
+}
+
+type processGroupOpts struct {
+	dynClient *dynamic.DynamicClient
+	col       *Collection
+	group     *metav1.APIResourceList
+}
+
+func (d *Detector) processGroup(ctx context.Context, opts processGroupOpts) {
+	filters := BuildFilters()
+
+rl:
+	for _, resource := range opts.group.APIResources {
+		gv, err := schema.ParseGroupVersion(opts.group.GroupVersion)
+		if err != nil {
+			return
+		}
+
+		gvr := schema.GroupVersionResource{
+			Group:    gv.Group,
+			Version:  gv.Version,
+			Resource: resource.Name,
+		}
+
+		for _, skip := range getSkipList() {
+			if gvr == skip {
+				continue rl
+			}
+		}
+
+		if !slices.Contains(resource.Verbs, "list") {
+			continue
+		}
+
+		restAPI := opts.dynClient.Resource(gvr).Namespace("")
+
+		gvrList, err := restAPI.List(ctx, metav1.ListOptions{})
+		if err != nil {
+			continue
+		}
+
+	il:
+		for _, item := range gvrList.Items {
+			for _, filter := range filters {
+				if filter(item) {
+					continue il
+				}
+			}
+			opts.col.Push(item)
+		}
+	}
 }
