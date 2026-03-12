eb573417fd0d4807a961fee831f6fb3fdbdad6fc
Henry Whitaker <henry.whitaker@commify.com>
2026-03-12 14:10:32 +0000

    feat: add functionality to pass cluster connection config via cli
    Signed-off-by: Henry Whitaker <henry.whitaker@commify.com>

--- internal/k8s/client.go
+++ internal/k8s/client.go
@@ -8,38 +8,47 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
 func init() {
 	rest.SetDefaultWarningHandler(rest.NoWarnings{})
 }
 
-func NewClient(kubeConfigPath string) (*kubernetes.Clientset, error) {
-	config, err := getConfig(kubeConfigPath)
+type ConnectionParams struct {
+	KubeconfigPath           string
+	URL                      string
+	ClientKeyPath            string
+	ClientCertificatePath    string
+	ClusterCACertificatePath string
+}
+
+func NewClient(params ConnectionParams) (*kubernetes.Clientset, error) {
+	config, err := getConfig(params)
 	if err != nil {
 		return nil, err
 	}
 	return kubernetes.NewForConfig(config)
 }
 
-func NewDynamicClient(kubeConfigPath string) (*dynamic.DynamicClient, error) {
-	config, err := getConfig(kubeConfigPath)
+func NewDynamicClient(params ConnectionParams) (*dynamic.DynamicClient, error) {
+	config, err := getConfig(params)
 	if err != nil {
 		return nil, err
 	}
 	return dynamic.NewForConfig(config)
 }
 
-func getConfig(kubeConfigPath string) (*rest.Config, error) {
+func getConfig(params ConnectionParams) (*rest.Config, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		path, err := resolveConfigPath(kubeConfigPath)
-		if err != nil {
-			return nil, err
+		builder := buildKubeconfigConfig
+		if params.URL != "" {
+			builder = buildDefinedConfig
 		}
-		// Grab it from kubeconfig if we can't get an in-cluster config
-		config, err = clientcmd.BuildConfigFromFlags("", path)
 
+		// Grab it from kubeconfig if we can't get an in-cluster config
+		config, err = builder(params)
 		if err != nil {
 			return nil, err
 		}
@@ -48,13 +57,47 @@ func getConfig(kubeConfigPath string) (*rest.Config, error) {
 	return config, nil
 }
 
+func buildDefinedConfig(params ConnectionParams) (*rest.Config, error) {
+	return clientcmd.BuildConfigFromKubeconfigGetter("", func() (*api.Config, error) {
+		return &api.Config{
+			Clusters: map[string]*api.Cluster{
+				"cluster": {
+					Server:               params.URL,
+					CertificateAuthority: params.ClusterCACertificatePath,
+				},
+			},
+			Contexts: map[string]*api.Context{
+				"cluster": {
+					Cluster:  "cluster",
+					AuthInfo: "cluster",
+				},
+			},
+			AuthInfos: map[string]*api.AuthInfo{
+				"cluster": {
+					ClientCertificate: params.ClientCertificatePath,
+					ClientKey:         params.ClientKeyPath,
+				},
+			},
+			CurrentContext: "cluster",
+		}, nil
+	})
+}
+
+func buildKubeconfigConfig(params ConnectionParams) (*rest.Config, error) {
+	path, err := resolveConfigPath(params.KubeconfigPath)
+	if err != nil {
+		return nil, err
+	}
+	return clientcmd.BuildConfigFromFlags("", path)
+}
+
 func resolveConfigPath(path string) (string, error) {
 	if strings.Contains(path, "~") {
 		home, err := os.UserHomeDir()
 		if err != nil {
 			return "", err
 		}
-		path = strings.Replace(path, "~", home, -1)
+		path = strings.ReplaceAll(path, "~", home)
 	}
 
 	return path, nil
